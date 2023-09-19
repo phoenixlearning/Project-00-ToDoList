6562f98ec07349fffb678dbd33f5c022bd1125eb
Jioh19 <jioh1982@gmail.com>
2023-09-19 12:06:30 -0300

    add localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+const tareas = localStorage.getItem("Tareas");
+
+if(tareas) {
+	document.querySelector(".container-card").innerHTML = tareas;
+}
+
 function addTask(e) {
 	const inputs = document.querySelectorAll(".input-form");
 	const tarea = inputs[0].value.trim();
@@ -17,25 +23,32 @@ function addTask(e) {
 					<p class='bx bx-edit-alt bx-tada-hover' onclick='editTask(event)'></p>
                     <p class='bx bxs-trash bx-tada-hover' onclick='deleteTask(event)'></p>
 				</div>`;
-	} 
+	}
+	setItem(containerCard.innerHTML);
 	cleanInputs(inputs);
 }
 
 function deleteTask(e) {
 	e.target.parentElement.remove();
+	setItem(document.querySelector(".container-card").innerHTML);
 }
 
 function editTask(e) {
 	const tarea = e.target.parentElement.querySelector(".tarea").innerHTML;
 	const codigo = e.target.parentElement.querySelector(".codigo").innerHTML;
-	const descripcion = e.target.parentElement.querySelector(".descripcion").innerHTML;
+	const descripcion =
+		e.target.parentElement.querySelector(".descripcion").innerHTML;
 	const inputs = document.querySelectorAll(".input-form");
 	inputs[0].value = tarea;
 	inputs[1].value = codigo;
 	inputs[2].value = descripcion;
-	deleteTask(e)
+	deleteTask(e);
 }
 
 function cleanInputs(inputs) {
 	inputs.forEach((input) => (input.value = ""));
+}
+
+function setItem(tasks) {
+	localStorage.setItem("Tareas", tasks);
 }
