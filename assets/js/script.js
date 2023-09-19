6c90c90f06aee2df69b7ccd532a3d1e6b94910f7
Jioh19 <jioh1982@gmail.com>
2023-09-19 12:18:19 -0300

    add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
+//* Inicialización, verifica la existencia del localStorage para cargar
 const tareas = localStorage.getItem("Tareas");
-
-if(tareas) {
+if (tareas) {
 	document.querySelector(".container-card").innerHTML = tareas;
 }
 
+//* Función vista en clases que agrega una tarea. Además guarda los cambios en el 
+//* localStorage con la función setItem. Invoca a cleanInputs para limpiar el formulario
 function addTask(e) {
 	const inputs = document.querySelectorAll(".input-form");
 	const tarea = inputs[0].value.trim();
 	const codigo = inputs[1].value.trim();
 	const descripcion = inputs[2].value.trim();
-
 	const containerCard = document.querySelector(".container-card");
 
 	e.preventDefault();
@@ -28,11 +29,15 @@ function addTask(e) {
 	cleanInputs(inputs);
 }
 
+//* Función parecida a la vista en clases, solo que no usa listeners. Actualiza el
+//* localStorage cuando hay cambios.
 function deleteTask(e) {
 	e.target.parentElement.remove();
 	setItem(document.querySelector(".container-card").innerHTML);
 }
 
+//* Función para editar. No es una edición, sólo rescata los valores de la tarea para
+//* luego eliminarla. Finalmente agrega una nueva tarea.
 function editTask(e) {
 	const tarea = e.target.parentElement.querySelector(".tarea").innerHTML;
 	const codigo = e.target.parentElement.querySelector(".codigo").innerHTML;
@@ -45,10 +50,12 @@ function editTask(e) {
 	deleteTask(e);
 }
 
+//* Función que limpia el formulario.
 function cleanInputs(inputs) {
 	inputs.forEach((input) => (input.value = ""));
 }
 
+//* Función que actualiza el localStorage.
 function setItem(tasks) {
 	localStorage.setItem("Tareas", tasks);
 }
