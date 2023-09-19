8f87830498d731390ca68e0fecb4a3e410ab9a90
Jioh19 <jioh1982@gmail.com>
2023-09-19 11:58:29 -0300

    add editTask

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,32 @@ function addTask(e) {
 	if (tarea == "" || codigo == "" || descripcion == "") {
 		alert("Llenar todos los campos");
 	} else {
-		console.log(tarea, codigo, descripcion);
-
 		containerCard.innerHTML += `<div class="card">
-					<p>${tarea}</p>
-					<p>${codigo}</p>
-					<p>${descripcion}</p>
-                    <i class='bx bxs-trash' onclick='deleteTask(event)'>
+					<p class="tarea">${tarea}</p>
+					<p class="codigo">${codigo}</p>
+					<p class="descripcion">${descripcion}</p>
+					<p class='bx bx-edit-alt bx-tada-hover' onclick='editTask(event)'></p>
+                    <p class='bx bxs-trash bx-tada-hover' onclick='deleteTask(event)'></p>
 				</div>`;
-	}
+	} 
+	cleanInputs(inputs);
 }
 
 function deleteTask(e) {
-    e.target.parentElement.remove();
+	e.target.parentElement.remove();
+}
+
+function editTask(e) {
+	const tarea = e.target.parentElement.querySelector(".tarea").innerHTML;
+	const codigo = e.target.parentElement.querySelector(".codigo").innerHTML;
+	const descripcion = e.target.parentElement.querySelector(".descripcion").innerHTML;
+	const inputs = document.querySelectorAll(".input-form");
+	inputs[0].value = tarea;
+	inputs[1].value = codigo;
+	inputs[2].value = descripcion;
+	deleteTask(e)
+}
+
+function cleanInputs(inputs) {
+	inputs.forEach((input) => (input.value = ""));
 }
