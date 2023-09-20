1d8d6c579a509658d3be69b2cbd02af6335b22cb
Jioh19 <jioh1982@gmail.com>
2023-09-19 22:48:22 -0300

    fix listaTareas y mas sass

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,67 @@
 //* Inicialización, verifica la existencia del localStorage para cargar
 const tareas = localStorage.getItem("Tareas");
+let listaTareas = [];
+let id = 0;
 if (tareas) {
-	document.querySelector(".container-card").innerHTML = tareas;
-}
+	console.log(tareas);
+	listaTareas = JSON.parse(tareas);
+	console.log(listaTareas);
+	updateList();
+} 
 
-//* Función vista en clases que agrega una tarea. Además guarda los cambios en el 
+//* Función vista en clases que agrega una tarea. Además guarda los cambios en el
 //* localStorage con la función setItem. Invoca a cleanInputs para limpiar el formulario
 function addTask(e) {
 	const inputs = document.querySelectorAll(".input-form");
-	const tarea = inputs[0].value.trim();
+	const nombre = inputs[0].value.trim();
 	const codigo = inputs[1].value.trim();
 	const descripcion = inputs[2].value.trim();
-	const containerCard = document.querySelector(".container-card");
 
 	e.preventDefault();
-	if (tarea == "" || codigo == "" || descripcion == "") {
+	if (nombre == "" || codigo == "" || descripcion == "") {
 		alert("Llenar todos los campos");
 	} else {
-		containerCard.innerHTML += `<div class="card">
-					<p class="tarea">${tarea}</p>
-					<p class="codigo">${codigo}</p>
-					<p class="descripcion">${descripcion}</p>
-					<p class='bx bx-edit-alt bx-tada-hover' onclick='editTask(event)'></p>
-                    <p class='bx bxs-trash bx-tada-hover' onclick='deleteTask(event)'></p>
-				</div>`;
+		const tarea = { id, nombre, codigo, descripcion };
+		id++;
+		listaTareas.push(tarea);
 	}
-	setItem(containerCard.innerHTML);
+	updateList();
 	cleanInputs(inputs);
 }
 
+function updateList() {
+	const containerCard = document.querySelector(".container-card");
+	containerCard.innerHTML = "";
+	listaTareas.forEach((tarea) => {
+		const { id, nombre, codigo, descripcion } = tarea;
+		containerCard.innerHTML += `<div class="card">
+					<p class="id hidden" value=${id}></p>
+					<p class="card-content nombre">${nombre}</p>
+					<p class="card-content codigo">${codigo}</p>
+					<p class="card-content descripcion">${descripcion}</p>
+					<p class='bx bx-edit-alt bx-tada-hover' onclick='editTask(event)'></p>
+					<p class='bx bxs-trash bx-tada-hover' onclick='deleteTask(event)'></p>
+					</div>`;
+	});
+	setItem(JSON.stringify(listaTareas))
+}
 //* Función parecida a la vista en clases, solo que no usa listeners. Actualiza el
 //* localStorage cuando hay cambios.
 function deleteTask(e) {
-	e.target.parentElement.remove();
-	setItem(document.querySelector(".container-card").innerHTML);
+	const id = e.target.parentElement.querySelector(".id").getAttribute("value");
+	listaTareas = listaTareas.filter(tarea => tarea.id != id);
+	updateList();
 }
 
-//* Función para editar. No es una edición real, sólo rescata los valores de la tarea para
-//* luego eliminarla. Finalmente hay agregar una nueva tarea.
+//* Función para editar. No es una edición real, sólo rescata los valores de la nombre para
+//* luego eliminarla. Finalmente hay agregar una nueva nombre.
 function editTask(e) {
-	const tarea = e.target.parentElement.querySelector(".tarea").innerHTML;
+	const nombre = e.target.parentElement.querySelector(".nombre").innerHTML;
 	const codigo = e.target.parentElement.querySelector(".codigo").innerHTML;
 	const descripcion =
 		e.target.parentElement.querySelector(".descripcion").innerHTML;
 	const inputs = document.querySelectorAll(".input-form");
-	inputs[0].value = tarea;
+	inputs[0].value = nombre;
 	inputs[1].value = codigo;
 	inputs[2].value = descripcion;
 	deleteTask(e);
